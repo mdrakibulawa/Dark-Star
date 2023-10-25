5eb15819699c66dea1981f7a695da915bbbfb376
mdrakibul369666 <143260241+mdrakibul369666@users.noreply.github.com>
2023-10-25 13:55:26 +0000

    fully ready

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express()
 const home =  require("./routes/home.js") ;
 
+
+app.use(express.static('public'))
+
 app.use('' , home)
+app.set('view engine' , 'ejs')
+
+
+
 
 const port = process.env.PORT || '3000'
 app.listen(port , ()=>{
--- controllers/homeControllers.js
+++ controllers/homeControllers.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 const homeController = (req, res)=>{
-    res.sendFile(path.join(process.cwd() , 'views' , 'index.html'))
+    res.render('index.ejs')
 }
 
 module.exports = homeController;
