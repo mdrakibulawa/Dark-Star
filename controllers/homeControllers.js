980e1fc223a782f233268afda594b8d57a7d216a
mdrakibul369666 <143260241+mdrakibul369666@users.noreply.github.com>
2024-01-10 12:21:02 +0000

    nabver added

--- controllers/homeControllers.js
+++ controllers/homeControllers.js
@@ -1,7 +1,11 @@
 const path = require('path')
 
 const homeController = (req, res)=>{
-    res.render('home.ejs')
+    const content = {
+        title : "Home Page 777",
+        css : "css/navber.css"
+    }
+    res.render('home.ejs', content)
 }
 
 module.exports = homeController;
